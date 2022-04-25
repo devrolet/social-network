3f30f8dd7e7b7b69b098ad5c394980e43d337024
Chet Hill <chetdhill@gmail.com>
2022-04-25 18:03:31 -0400

    Create Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopnavComponent } from './components/topnav/topnav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+// Angular Material Imports
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+// Application Imports
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopnavComponent
+    TopnavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
