f950f956569a2c9ecaeeb6340ad6d8fdb334b66c
Chet Hill <chetdhill@gmail.com>
2022-04-25 17:32:23 -0400

    Create Top Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './components/topnav/topnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent
   ],
   imports: [
     BrowserModule,
