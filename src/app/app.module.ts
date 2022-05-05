14e24d7aafbc4b295582fa9628c9266984ad6651
Chet Hill <chetdhill@gmail.com>
2022-05-05 16:35:22 -0400

    Login Logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HttpClientModule } from '@angular/common/http'
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 // Application Imports
 import { LoginComponent } from './pages/login/login.component';
@@ -32,7 +33,8 @@ import { CreateAccountComponent } from './pages/create-account/create-account.co
     MatInputModule,
     ReactiveFormsModule, 
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,10 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder, 
+    public userService: UserService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
@@ -18,5 +23,27 @@ export class LoginComponent implements OnInit {
     email:['', [Validators.required, Validators.email]],
     password:['', [Validators.required, Validators.minLength(6)]]
   });
+
+  login() {
+    this.userService.getUser(this.loginForm.value.email)
+      .then((res: any)=> {
+        console.log(res);
+        if(res.length == 0){
+          console.log('Account does not exist');
+          this.snackBar.open('Account does not exist', 'OK');
+        }else {
+          if(res[0].password === this.loginForm.value.password){
+            console.log('matched');
+            this.snackBar.open('Logged In Successfully', 'OK');
+          }else {
+            console.log('Incorrect Password');
+            this.snackBar.open('Incorrect Password', 'OK');
+          }
+        }
+      })
+      .catch((err)=> {
+        console.log(err);
+      })
+  }
 
 }
